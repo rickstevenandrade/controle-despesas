bf580c4463463f25a643981a823b1da006e45b60
Rick Steven <rickssandrade@gmail.com>
2024-11-22 15:35:58 -0300

    Subindo projeto para Render

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
+
+
 # Instâncias globais
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
-def create_app():
+def create_app():    
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sua_chave_secreta'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))    # Configura o Flask para ler a variável de ambiente PORT (para produção)
+
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
